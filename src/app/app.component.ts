08e4e17c341d227c19d685030e8d6d94e8d21f3e
pyp <pyp@github.com>
2018-03-13 16:05:43 -0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { Network } from '@ionic-native/network';
+
 import { GlobalService } from '../shared/services/global.service';
 import { LoginPage } from '../pages/login/login';
 
