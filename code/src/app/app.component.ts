165516f9987e08e51370cd6cd3fcb3a1f07302f6
pyp <pyp@github.com>
2018-07-21 22:25:53 -0300

    fix status bar

--- code/src/app/app.component.ts
+++ code/src/app/app.component.ts
@@ -36,59 +36,56 @@ export class KeymasterApp implements OnInit, OnDestroy {
 
       this.platform.ready().then(() => {
          console.log('platform ready....')
-         setTimeout(() => {
-            console.log('starting update validation......')
-            this.codePush.sync({}, (progress) => {
-               this.zone.run(() => {
-                  this.prog = Math.floor(progress.receivedBytes / progress.totalBytes * 100)
-                  console.log('progress: ', this.prog)
-               })
-            }).subscribe(status => {
-               switch (status) {
-                  case SyncStatus.CHECKING_FOR_UPDATE:
-                     console.log('CHECKING_FOR_UPDATE: ', SyncStatus.CHECKING_FOR_UPDATE)
-                     this.appSrv.message('checking for update', '')
-                     break;
-                  case SyncStatus.AWAITING_USER_ACTION:
-                     console.log('AWAITING_USER_ACTION: ', SyncStatus.AWAITING_USER_ACTION)
-                     this.appSrv.basicAlert('waiting for user input')
-                     break;
-                  case SyncStatus.IN_PROGRESS:
-                     console.log('IN_PROGRESS: ', SyncStatus.IN_PROGRESS)
-                     this.appSrv.message('update in progress')
-                     break;
-                  case SyncStatus.DOWNLOADING_PACKAGE:
-                     console.log('DOWNLOADING_PACKAGE: ', SyncStatus.DOWNLOADING_PACKAGE)
-                     this.appSrv.message('downloading package')
-                     this.zone.run(() => {
-                        this.showProgressBar = true
-                        console.log('showProgressBar: ', this.showProgressBar)
-                     })
-                     break;
-                  case SyncStatus.UP_TO_DATE:
-                     console.log('UP_TO_DATE: ', SyncStatus.UP_TO_DATE)
-                     this.appSrv.message('app up to date')
-                     break;
-                  case SyncStatus.INSTALLING_UPDATE:
-                     console.log('INSTALLING_UPDATE: ', SyncStatus.INSTALLING_UPDATE)
-                     this.appSrv.message('installing update')
-                     break;
-                  case SyncStatus.UPDATE_IGNORED:
-                     console.log('UPDATE_IGNORED: ', SyncStatus.UPDATE_IGNORED)
-                     this.appSrv.message('update ignored')
-                     break;
-                  case SyncStatus.UPDATE_INSTALLED:
-                     console.log('UPDATE_INSTALLED: ', SyncStatus.UPDATE_INSTALLED)
-                     this.codePush.restartApplication();
-                     this.appSrv.basicAlert('update installed, restart app!')
-                  case SyncStatus.ERROR:
-                     this.appSrv.message('SyncStatus.ERROR....')
-                     console.log('SyncStatus.ERROR: ', SyncStatus.ERROR)
-                     break;
-               }
+         console.log('starting update validation......')
+         this.codePush.sync({}, (progress) => {
+            this.zone.run(() => {
+               this.prog = Math.floor(progress.receivedBytes / progress.totalBytes * 100)
+               console.log('progress: ', this.prog)
             })
-         }, 3000);
-
+         }).subscribe(status => {
+            switch (status) {
+               case SyncStatus.CHECKING_FOR_UPDATE:
+                  console.log('CHECKING_FOR_UPDATE: ', SyncStatus.CHECKING_FOR_UPDATE)
+                  this.appSrv.message('checking for update', '')
+                  break;
+               case SyncStatus.AWAITING_USER_ACTION:
+                  console.log('AWAITING_USER_ACTION: ', SyncStatus.AWAITING_USER_ACTION)
+                  this.appSrv.basicAlert('waiting for user input')
+                  break;
+               case SyncStatus.IN_PROGRESS:
+                  console.log('IN_PROGRESS: ', SyncStatus.IN_PROGRESS)
+                  this.appSrv.message('update in progress')
+                  break;
+               case SyncStatus.DOWNLOADING_PACKAGE:
+                  console.log('DOWNLOADING_PACKAGE: ', SyncStatus.DOWNLOADING_PACKAGE)
+                  this.appSrv.message('downloading package')
+                  this.zone.run(() => {
+                     this.showProgressBar = true
+                     console.log('showProgressBar: ', this.showProgressBar)
+                  })
+                  break;
+               case SyncStatus.UP_TO_DATE:
+                  console.log('UP_TO_DATE: ', SyncStatus.UP_TO_DATE)
+                  this.appSrv.message('app up to date')
+                  break;
+               case SyncStatus.INSTALLING_UPDATE:
+                  console.log('INSTALLING_UPDATE: ', SyncStatus.INSTALLING_UPDATE)
+                  this.appSrv.message('installing update')
+                  break;
+               case SyncStatus.UPDATE_IGNORED:
+                  console.log('UPDATE_IGNORED: ', SyncStatus.UPDATE_IGNORED)
+                  this.appSrv.message('update ignored')
+                  break;
+               case SyncStatus.UPDATE_INSTALLED:
+                  console.log('UPDATE_INSTALLED: ', SyncStatus.UPDATE_INSTALLED)
+                  this.codePush.restartApplication();
+                  this.appSrv.basicAlert('update installed, restart app!')
+               case SyncStatus.ERROR:
+                  this.appSrv.message('SyncStatus.ERROR....')
+                  console.log('SyncStatus.ERROR: ', SyncStatus.ERROR)
+                  break;
+            }
+         })
          // Okay, so the platform is ready and our plugins are available.
          // Here you can do any higher level native things you might need.
          this.statusBar.styleDefault()
